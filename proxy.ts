e66be259782398894e64db79d72184bafe0d2fa0
darseen <darseen.dev@gmail.com>
2026-05-05 16:48:08 +0300

    Add proxy for authorization

---
+++ proxy.ts
@@ -0,0 +1,48 @@
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "./db";
+import { usersTable } from "./db/schema";
+import { verifyToken } from "./utils/jwt";
+
+export async function proxy(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+  const { pathname } = request.nextUrl;
+
+  // If the user has a token and is on the homepage, redirect to the dashboard.
+  if (token && pathname === "/") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  // If the user is trying to access a protected dashboard route without a token, redirect to the homepage.
+  if (!token && pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // If a token exists for a protected route, verify it.
+  if (token && pathname.startsWith("/dashboard")) {
+    // verify token
+    const payload = await verifyToken(token);
+    if (!payload) {
+      // Token is invalid, redirect to the homepage and clear the invalid token.
+      const response = NextResponse.redirect(new URL("/", request.url));
+      response.cookies.delete("token");
+      return response;
+    }
+
+    // check if user exists in the database
+    const user = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.id, payload.user.id));
+    if (!user) {
+      // Token is invalid, redirect to the homepage and clear the invalid token.
+      const response = NextResponse.redirect(new URL("/", request.url));
+      response.cookies.delete("token");
+      return response;
+    }
+  }
+}
+
+export const config = {
+  matcher: ["/", "/dashboard/:path*"],
+};
