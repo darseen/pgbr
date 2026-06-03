b9d4c43a8b6331f5e3ebdb46d558c748b4f9fcab
darseen <darseen.dev@gmail.com>
2026-06-03 16:00:42 +0300

    Create pgbr data path if it doesn't exist

--- db/index.ts
+++ db/index.ts
@@ -1,12 +1,17 @@
 import { getPgbrDataPath } from "@/utils";
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
+import { existsSync, mkdirSync } from "node:fs";
 import path from "node:path";
 import * as schema from "./schema";
 
-const pgbrDataPath = path.join(getPgbrDataPath(), "pgbr.db");
+const pgbrDataPath = getPgbrDataPath();
 
-const sqlite = new Database(pgbrDataPath);
+if (!existsSync(pgbrDataPath)) {
+  mkdirSync(pgbrDataPath, { recursive: true });
+}
+
+const sqlite = new Database(path.join(pgbrDataPath, "pgbr.db"));
 
 export const db = drizzle({
   client: sqlite,
