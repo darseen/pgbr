1fdbb06c08ca97beb96fd0d06bd3cd41849f0a70
darseen <darseen.dev@gmail.com>
2026-06-08 21:30:18 +0300

    Refactor ecryption.ts

--- utils/encryption.ts
+++ utils/encryption.ts
@@ -3,16 +3,9 @@ import crypto from "node:crypto";
 const ALGORITHM = "aes-256-gcm";
 const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
 
-if (!ENCRYPTION_KEY) {
-  throw new Error("Missing ENCRYPTION_KEY environment variable.");
-}
-
-// automatically convert any length string into a 32-byte buffer using SHA-256
-const key = crypto.createHash("sha256").update(ENCRYPTION_KEY).digest();
-
 export function encrypt(text: string): string {
   const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
+  const cipher = crypto.createCipheriv(ALGORITHM, getKey(), iv);
 
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
@@ -32,11 +25,21 @@ export function decrypt(encryptedData: string): string {
   const iv = Buffer.from(ivHex, "hex");
   const authTag = Buffer.from(authTagHex, "hex");
 
-  const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
+  const decipher = crypto.createDecipheriv(ALGORITHM, getKey(), iv);
   decipher.setAuthTag(authTag);
 
   let decrypted = decipher.update(encryptedText, "hex", "utf8");
   decrypted += decipher.final("utf8");
 
   return decrypted;
+}
+
+function getKey() {
+  if (!ENCRYPTION_KEY) {
+    throw new Error("Missing ENCRYPTION_KEY environment variable.");
+  }
+
+  // automatically convert any length string into a 32-byte buffer using SHA-256
+  const key = crypto.createHash("sha256").update(ENCRYPTION_KEY).digest();
+  return key;
 }
